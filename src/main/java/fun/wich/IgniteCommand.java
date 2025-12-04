22ed2dfb5d4db1a4109700523dc8b4f8cc6b82fb
OfficePigeon <ava@wich.fun>
2025-12-04 16:06:48 -0500

    1.19 initial

--- src/main/java/fun/wich/IgniteCommand.java
+++ src/main/java/fun/wich/IgniteCommand.java
@@ -5,18 +5,19 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import net.fabricmc.api.ModInitializer;
 
-import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.command.CommandRegistryAccess;
 import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.entity.Entity;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.text.TranslatableText;
+import net.minecraft.text.Text;
 
 import java.util.Collection;
 
 public class IgniteCommand implements ModInitializer {
-	@Override public void onInitialize() { CommandRegistrationCallback.EVENT.register((IgniteCommand::register)); }
-	public static void register(CommandDispatcher<ServerCommandSource> dispatcher, boolean dedicated) {
+	@Override public void onInitialize() { CommandRegistrationCallback.EVENT.register(IgniteCommand::register); }
+	public static void register(CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess access, CommandManager.RegistrationEnvironment environment) {
 		dispatcher.register(CommandManager.literal("ignite")
 				.requires(source -> source.hasPermissionLevel(2))
 				.executes((context) -> execute(context.getSource(), ImmutableList.of(context.getSource().getEntityOrThrow()), 10))
@@ -27,8 +28,8 @@ public class IgniteCommand implements ModInitializer {
 	}
 	private static int execute(ServerCommandSource source, Collection<? extends Entity> targets, int duration) {
 		for (Entity entity : targets) entity.setOnFireFor(duration);
-		if (targets.size() == 1) source.sendFeedback(new TranslatableText("commands.ignite.success.single", targets.iterator().next().getDisplayName()), true);
-		else source.sendFeedback(new TranslatableText("commands.ignite.success.multiple", targets.size()), true);
+		if (targets.size() == 1) source.sendFeedback(Text.translatable("commands.ignite.success.single", targets.iterator().next().getDisplayName()), true);
+		else source.sendFeedback(Text.translatable("commands.ignite.success.multiple", targets.size()), true);
 		return targets.size();
 	}
 }
